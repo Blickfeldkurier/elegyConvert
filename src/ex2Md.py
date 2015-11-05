ac6930ea6d476c9f53e67fffc8a632d752561320
Frederick Bullik <Frederick.Bullik@Posteo.de>
2015-11-05 02:17:32 +0100

    ex2Md.py works

--- src/ex2Md.py
+++ src/ex2Md.py
@@ -1,4 +1,26 @@
 #!/usr/bin/env python
 
+import argparse
+import json
+import sys
+
+def genMarkdown():
+    retVal = ""
+    lines = sys.stdin.readline()
+    obj = json.loads(lines)
+    retVal += "# " + obj["author"] + ": " + obj["title"] +  '\n\n'
+    for elem in obj['text']:
+        retVal += elem + '\n\n'
+    return(retVal)
+
 if __name__ == "__main__":
-    print("foo")
+    parser = argparse.ArgumentParser(prog='ex2MD.py')
+    parser.add_argument("-o", "--output", dest='outpath', help="Path to output file")
+    args = parser.parse_args()
+
+    retVal = genMarkdown()
+    if(args.outpath):
+        with open(args.outpath, 'w') as file:
+            file.write(retVal)
+    else:
+        print(retVal)
--- src/ex2Tex.py
+++ src/ex2Tex.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python
 
+import argparse
+import json
+import sys
+
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(prog='ex2Tex.py')
+    parser.add_argument("-o", "--output", dest='outpath', help="Path to output file")
+    args = parser.parse_args()
     print("foo")
